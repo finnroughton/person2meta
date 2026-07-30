d86244405c3b7fee11b3854cb352e6a719806aea
finnroughton <finn.roughton@gmail.com>
2026-07-30 13:21:36 -0700

    Create convert_natalie.bat

---
+++ convert_natalie.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Blender Foundation\Blender 5.2\blender.exe" --background --python "%~dp0convert_obj_to_fbx.py" -- "C:\Users\BrianBurritt\Downloads\New Folder\celeb\celeb\natalie\neutral.obj" "C:\Users\BrianBurritt\Downloads\New Folder\celeb\celeb\natalie\natalie_head.fbx"
+pause
